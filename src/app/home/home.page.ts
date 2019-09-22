8e6fa478376407a54cdb92669126730a15cf8932
Eric Cobianchi <ericcobianchi@hotmail.com>
2019-09-22 13:55:16 -0300

    bug: fix ranking position

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,7 +16,15 @@ export class HomePage {
   public async getGameParsed() {
     await this.gameService.getGame().subscribe(gameParsed => {
       this.ranking = Object.entries(gameParsed.ranking);
-      console.log(this.ranking);
+      this.ranking = this.ranking.sort((a, b) => {
+        if (a[1] < b[1]) {
+          return 1;
+        }
+        if (a[1] > b[1]) {
+          return -1;
+        }
+        return 0;
+      });
     });
   }
 }
