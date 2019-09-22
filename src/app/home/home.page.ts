a14f47035d4b4d1486897d78e4e6ea5c87aa5d20
Eric Cobianchi <ericcobianchi@hotmail.com>
2019-09-22 13:26:24 -0300

    feat: add game page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { GameService } from '../service/game.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  public ranking = [];
 
-  constructor() {}
+  constructor(private gameService: GameService) {
+    this.getGameParsed();
+  }
 
+  public async getGameParsed() {
+    await this.gameService.getGame().subscribe(gameParsed => {
+      this.ranking = Object.entries(gameParsed.ranking);
+      console.log(this.ranking);
+    });
+  }
 }
